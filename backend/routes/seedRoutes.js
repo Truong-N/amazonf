8707df987bec287324428deef274fd3f36caf778
XTruongN <tn647353@gmail.com>
2023-02-23 23:23:35 -0500

    Video-21

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import Product from '../models/productModel.js';
 import data from '../data.js';
+import User from '../models/userModel.js';
 
 const seedRouter = express.Router();
 seedRouter.get('/', async (req, res) => {
   await Product.remove({});
   const CreatedProducts = await Product.insertMany(data.products);
-  res.send({ CreatedProducts });
+  await User.remove({});
+  const CreatedUsers = await User.insertMany(data.users);
+  res.send({ CreatedProducts, CreatedUsers });
 });
 export default seedRouter;
